aa74fb5d4f19c17ea0192fad90ee2d1560543cb3
imilano <imilano@users.noreply.github.com>
2022-05-19 04:05:31 +0000

    deploy: f54fe6124daa689751a874b74aa02cc4bda86dd7

--- docs/algs/basic/msort/merge_sort.go
+++ docs/algs/basic/msort/merge_sort.go
@@ -1,11 +1,26 @@
 package msort
 
-func MergeSort(arr []int, left, right int) {
+// 自顶向下
+func MergeSortUpToDown(arr []int, left, right int) {
 	if left < right {
 		mid := (left + right) / 2
-		MergeSort(arr, left, mid)
-		MergeSort(arr, mid+1, right)
+		MergeSortUpToDown(arr, left, mid)
+		MergeSortUpToDown(arr, mid+1, right)
 		Merge(arr, left, mid, right)
+	}
+}
+
+// 自底向上
+func MergeSortDownToUp(arr []int) {
+	size := len(arr)
+	if size <= 1 {
+		return
+	}
+
+	for i := 1; i < size; i *= 2 { // 间隔，就是每个子数组的长度
+		for j := 0; j < size; j += 2*i { // 两两合并， j 表示第一个子数组的开始索引
+			Merge(arr, j, j+i-1, min(j+2*i-1,  size-1)) // 对于长度不满足 2 的 x 次幂的数组，j+2*i-1 可能会超过数组长度，所以需要控制一下
+		}
 	}
 }
 
