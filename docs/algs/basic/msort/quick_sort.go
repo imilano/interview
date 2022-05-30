850f96298f003f64d124b83aba95f48910c208ce
imilano <imilano@users.noreply.github.com>
2022-05-30 12:34:28 +0000

    deploy: 1aaae86156f0504e444f7f1d23ee4a2936a2439c

--- docs/algs/basic/msort/quick_sort.go
+++ docs/algs/basic/msort/quick_sort.go
@@ -9,9 +9,11 @@ func QuickSort(arr []int, left, right int) {
 }
 
 
+// 快速选择算法
 func partition(arr []int, left, right int) int {
 	pivot := left
 	index := pivot + 1
+	// 把比 pivot 小的元素都放在左边，也就是说，除了第一个 pivot，index 左边的元素都比 pivot 小（不包括 index）
 	for i := index; i <= right; i++ {
 		if arr[i] < arr[pivot] {
 			arr[i], arr[index] = arr[index], arr[i]
@@ -19,6 +21,7 @@ func partition(arr []int, left, right int) int {
 		}
 	}
 
+	// 把 index 的前一个（比 pivot 小）和 pivot 交换，这样 pivot 左边的元素都比 pivot 要小了
 	arr[pivot], arr[index -1] = arr[index-1], arr[pivot]
 	return index -1 
 }
